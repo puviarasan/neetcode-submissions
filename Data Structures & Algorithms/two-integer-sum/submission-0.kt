d416d38f3dffa935499467c62dd363f55d1642cc
puviarasan <37922829+puviarasan@users.noreply.github.com>
2026-04-16 16:28:11 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        var map = mutableMapOf<Int,Int>()
+        for(i in nums.indices){
+            val diff = target - nums[i]
+            if(map.containsKey(diff)){
+                return intArrayOf(map[diff]!!,i)
+            }   
+            else{
+                map[nums[i]] = i
+               
+            }   
+        }
+        return intArrayOf()
+    }
+}
